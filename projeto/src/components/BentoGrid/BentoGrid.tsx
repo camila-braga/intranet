1eac44f331df39b4dfa3332942da951f22ebdf4b
Camila Braga <milabragalima@hotmail.com>
2025-09-30 12:30:14 -0300

    rectangle button extra big tsx e css

--- projeto/src/components/BentoGrid/BentoGrid.tsx
+++ projeto/src/components/BentoGrid/BentoGrid.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import styles from './BentoGrid.module.css';
 import SquareButtonBig from '../SquareButtonBig/SquareButton_big';
 import RectangleButtonBig from '../RectangleButtonBig/RectangleButton_big';
-// importe outros botões aqui
+import RectangleButtonExtraBig from '../RectangleButtonExtraBig/RectangleButton_extraBig';
 
 export default function BentoGrid() {
   return (
     <div className={styles.grid}>
       <RectangleButtonBig iconSrc="/icon/bentoGrid/ponto.png" label="Controle de Ponto" />
+
       <SquareButtonBig iconSrc="/icon/bentoGrid/ceaf.png" label="CEAF" />
+
+      <RectangleButtonExtraBig />
+
       <SquareButtonBig iconSrc="/icon/bentoGrid/sei.png" label="SEI RN" />
+
       <SquareButtonBig iconSrc="/icon/bentoGrid/horus.png" label="HÓRUS" />
-      {/* outros botões */}
     </div>
   );
 }
---
+++ projeto/src/components/RectangleButtonExtraBig/RectangleButton_extraBig.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './RectangleButton_extraBig.module.css';
+
+export default function RectangleButtonExtraBig() {
+  return (
+    <button className={styles.button}>
+      
+    </button>
+  );
+}

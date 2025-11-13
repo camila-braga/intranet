5edf709e2ba2f7fa243ee42266e652b79aa65875
Camila Braga <milabragalima@hotmail.com>
2025-11-13 11:50:26 -0300

    Feat: Refatoração dos estilos dos componentes

--- projeto/src/components/buttons/RectangleButton_extraBig.tsx
+++ projeto/src/components/buttons/RectangleButton_extraBig.tsx
@@ -15,20 +15,13 @@ export default function RectangleButtonExtraBig({ iframeSrc, linkUrl }: Rectangl
   return (
     <div 
       className="
-        w-full h-full 
-        bg-white border border-black/15
-        shadow-[0_4px_12px_rgba(0,0,0,0.1)]
-        cursor-pointer 
-        overflow-hidden p-0
         flex items-center justify-center
-        transition-transform duration-200 ease-in-out
-        hover:scale-105
-        hover:bg-accent-light
+        button-base
 
-        rounded-2xl                      // Base, rounded: 16px
-        hd:rounded-2xl                   // HD, rounded: 16px
-        qhd:rounded-[1.25rem]            // QHD, rounded: 20px
-        4k:rounded-3xl                   // 4k, rounded: 24px
+        p-0                       // Base, padding: 0px
+        hd:p-0                    // HD, padding: 0px 
+        qhd:p-0                   // QHD, padding: 0px 
+        4k:p-0                    // 4K, padding: 0px
       "
       onClick={handleClick}
     >
@@ -44,20 +37,25 @@ export default function RectangleButtonExtraBig({ iframeSrc, linkUrl }: Rectangl
           w-[1600px]
           h-[1200px]
 
-          top-[-1px]         // Base, px
-          hd:top-[-44px]      // HD, px
+          rounded-2xl               // Base, rounded: 16px
+          hd:rounded-2xl            // HD, rounded: 16px
+          qhd:rounded-[1.25rem]     // QHD, rounded: 20px
+          4k:rounded-3xl            // 4K, rounded: 24px
+
+          top-[-1px]           // Base, px
+          hd:top-[-44px]       // HD, px
           qhd:top-[-104px]     // QHD, px
           4k:top-[-180px]      // 4k, px
 
           left-[-115px]         // Base, px
-          hd:left-[-48px]      // HD, px
+          hd:left-[-48px]       // HD, px
           qhd:left-[-136px]     // QHD, px
-          4k:left-[-80px]      // 4k, px
+          4k:left-[-80px]       // 4k, px
 
-          scale-[0.5]         // Base
-          hd:scale-[0.6]      // HD, px
-          qhd:scale-[0.9]     // QHD, px
-          4k:scale-[1.2]     // 4k, px
+          scale-[0.5]          // Base
+          hd:scale-[0.6]       // HD, px
+          qhd:scale-[0.9]      // QHD, px
+          4k:scale-[1.2]       // 4k, px
         "
         sandbox="allow-same-origin allow-scripts"
       />
